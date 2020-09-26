ef98fbb32f5c37a9f6567af24b39538b4e5bd2da
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-09-26 00:50:36 -0400

    commmit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+  'url': 'mongodb+srv://demo:demo@cluster0.84ovu.mongodb.net/barista?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  'dbName': 'barista'
+};
+//test
