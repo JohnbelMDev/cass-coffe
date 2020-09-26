54e7794f313202a24b8ff72f14ec20c2401750f5
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-09-26 00:52:20 -0400

    commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-  'url': 'mongodb+srv://demo:demo@cluster0.84ovu.mongodb.net/barista?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-  'dbName': 'barista'
+  'url' : 'mongodb+srv://Johnbel458:CPTAJLWIEEltRGus@cluster0.lbzzt.mongodb.net/Joshua?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  'dbName': 'John'
 };
 //test
